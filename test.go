b730d9ce8c3f1485a5d30f6d68308881036feba7
arjun <arjunravichandran11@gmail.com>
2020-02-10 18:40:25 +0530

    ads

--- test.go
+++ test.go
@@ -8,11 +8,9 @@ type person struct{
 
 func main(){
 
-    a := [5]person{person{21}, person{42}, person{43}, person{24}, person{25}}
-
-    fmt.Println(function1(a))
-    fmt.Println(function2(a))
-
+    a := [5]person{person{16}, person{31}, person{15}, person{18}, person{32}}
+     fmt.Println(function1(a))
+     fmt.Println(function2(a))
 
 }
 
@@ -28,13 +26,33 @@ for j := 0; i < len(a); i++ {
 return false
 }
 
+
 func function2(a [5] person)bool {
+   // a.sort()
+  //  fmt.Println(a)
+    m := make(map[string]int)
+    m["largest"] = a[0].age;
+    m["smallest"] = a[0].age;
 for i := 0; i < len(a); i++ {
-for j := 0; i < len(a); i++ {
-        if a[i].age>=2*a[j].age {
-            return true
-        }
+
+ if a[i].age>m["largest"] {
+        m["largest"]=a[i].age
+    } else if a[i].age<m["smallest"] {
+         m["smallest"]=a[i].age
+    } else {
+    }
+     if m["largest"]>=2*m["smallest"] {
+      return true
     }
 }
-return false
+
+ return false
+
 }
+
+
+
+
+
+
+
